97a322817c6d0431da5234d9f355665dc6c7a32e
Daniel-Alievsky <daniel.alievsky@gmail.com>
2016-09-20 15:15:42 +0300

    Grizzly: fixing problem with keystroke password; changing expiration from 1 to 10 years

--- grizzly-tests/src/test/resources/make_certificate.cmd
+++ grizzly-tests/src/test/resources/make_certificate.cmd
@@ -1,1 +1,1 @@
-"%JAVA_HOME%\bin\keytool" -genkey -keyalg RSA -alias self-signed -keystore ssl-test-keystore.jks -storepass password -validity 3650 -keysize 2048
+"%JAVA_HOME%\bin\keytool" -genkeypair -alias algart -keyalg RSA -keystore ssl-test-keystore.jks -keypass changeit -storepass changeit -validity 3650 -keysize 2048
